8576bcb27a7f4d0d915eb4698330a3150ae845b7
Yejin Kim <21800141@handong.edu>
2021-11-04 14:27:16 +0900

    url-basic functions finished

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,5 +12,25 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+
+void MainWindow::on_start_button_clicked()
+{
+    //ui->stackedWidget(start_page)->show()
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+
+void MainWindow::on_rules_button_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_return_button_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 
+
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -14,6 +15,14 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+
+    void on_start_button_clicked();
+
+    void on_rules_button_clicked();
+
+    void on_return_button_clicked();
 
 private:
     Ui::MainWindow *ui;
