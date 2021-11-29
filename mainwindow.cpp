6755137dc2913a8ae8b86f47e650bf91b333db4e
Yejin Kim <21800141@handong.edu>
2021-11-29 16:11:21 +0900

    include 10 moves version

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-int turn=0; //global variable to call on
+//int turn=0; //global variable to call on
 
 using namespace std;
 
@@ -12,12 +12,13 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    QPixmap choosing_player, starting_page, rules_page, ending_page, playing_page;
+    QPixmap choosing_player, starting_page, rules_page, ending_page, playing_page, choose_mode_page;
     choosing_player.load("C:/HGU/Grade4_Sem2/Programming2/reversiiii/tosize_2/choose_player.png"); //CHANGE IMAGE DIRECTORY HERE!
     starting_page.load("C:/HGU/Grade4_Sem2/Programming2/reversiiii/tosize_2/start_page.png");
     rules_page.load("C:/HGU/Grade4_Sem2/Programming2/reversiiii/tosize_2/rules_page.png");
     playing_page.load("C:/HGU/Grade4_Sem2/Programming2/reversiiii/tosize_2/playing_background.png");
     ending_page.load("C:/HGU/Grade4_Sem2/Programming2/reversiiii/tosize_2/ending_page.png");
+    choose_mode_page.load("C:/HGU/Grade4_Sem2/Programming2/reversiiii/tosize_2/choose_mode_page.png");
 
     //ui->player_status->show();
     ui->choose_player_label->setPixmap(choosing_player);
@@ -25,6 +26,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->rule_page_label->setPixmap(rules_page);
     ui->playing_background->setPixmap(playing_page);
     ui->ending_page_label->setPixmap(ending_page);
+    ui->choose_mode_background->setPixmap(choose_mode_page);
 
     QPushButton *button_slots_connect[8][8]; //creating slot names as array; define in header
     for(int i=0; i<8; ++i){
@@ -46,7 +48,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_start_button_clicked()
 {
-    ui->stackedWidget->setCurrentIndex(2);
+    ui->stackedWidget->setCurrentIndex(5);
 }
 
 void MainWindow::update_board_icons(){ //change icons; does not get until here!!!!!
@@ -72,7 +74,7 @@ void MainWindow::update_board_icons(){ //change icons; does not get until here!!
     }
 }
 
-void MainWindow::end_the_game(int player1_score, int player2_score)
+void MainWindow::end_the_game()
 {
     int winner;
     ui->stackedWidget->setCurrentIndex(4);
@@ -87,16 +89,24 @@ void MainWindow::end_the_game(int player1_score, int player2_score)
 
     }
 
-    ui->player1_final->setText(QString::number(player1_score));
-    ui->player2_final->setText(QString::number(player2_score));
-    ui->final_winner->setText(QString::number(winner));
+    if(winner==3){
+        ui->final_winner->setText("1&2");
+
+    }
+    else{
+        ui->player1_final->setText(QString::number(player1_score));
+        ui->player2_final->setText(QString::number(player2_score));
+        ui->final_winner->setText(QString::number(winner));
+    }
+
+
     //set winner label
 }
 
 void MainWindow::update_board_score() //updating score
 {
-    int player1_score=0;
-    int player2_score=0;
+    player1_score=0;
+    player2_score=0;
     int end_game=0;
 
     for(int i=0; i<8; ++i){
@@ -114,7 +124,7 @@ void MainWindow::update_board_score() //updating score
     ui->score_player2->setText(QString::number(player2_score));
 
     if(end_game==0){
-        end_the_game(player1_score, player2_score);
+        end_the_game();
     }
 
 }
@@ -293,8 +303,6 @@ void MainWindow::button_slots_pressed(){
         ui->warning_1->setPixmap(warning1);
         ui->warning_1->show();
 
-        //ui->warning_1->setText("Cannot place here!");
-        //ui->warning_2->setText("Cannot place here!");
 
 
         //setting label without qt designer and only comes out for warning signs
@@ -306,9 +314,6 @@ void MainWindow::button_slots_pressed(){
 
         ui->warning_1->setPixmap(warning1);
         ui->warning_1->show();
-        //ui->warning_1->setText("Cannot place here!");
-        //ui->warning_2->setText("Cannot place here!");
-
     }
 
     else{
@@ -354,6 +359,19 @@ void MainWindow::button_slots_pressed(){
 
     update_board_score(); //update scores
 
+
+    if(limit_mode==true){
+        if(starting_player==1){
+            if(turn==10)
+                end_the_game();
+
+        }
+        else{
+            if(turn==11)
+                end_the_game();
+        }
+    }
+
 }
 
 
@@ -390,12 +408,15 @@ void MainWindow::initialize_board()
 
 void MainWindow::on_choose_player1_clicked()
 {
+    starting_player=1;
     ui->stackedWidget->setCurrentIndex(3);
     QPixmap player1;
     player1.load("C:/HGU/Grade4_Sem2/Programming2/reversiiii/tosize_2/player1_status.png"); //CHANGE IMAGE DIRECTORY HERE!
 
     ui->player_status->setPixmap(player1);
     ui->player_status->show();
+
+
 
 
 
@@ -408,6 +429,7 @@ void MainWindow::on_choose_player1_clicked()
 
 void MainWindow::on_choose_player2_clicked()
 {
+    starting_player=2;
     ui->stackedWidget->setCurrentIndex(3);
     QPixmap player2;
     player2.load("C:/HGU/Grade4_Sem2/Programming2/reversiiii/tosize_2/player2_status.png"); //CHANGE IMAGE DIRECTORY HERE!
@@ -421,229 +443,21 @@ void MainWindow::on_choose_player2_clicked()
 }
 
 
-//===============================================
-//start of game code
-//===============================================
-
-/*
-
-//#include "widget.h"
-
-#include <array>
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <vector>
-
-using namespace std;
-
-#define NUM_ROWS 8
-#define NUM_COLS 8
-#define EMPTY 0
-#define WHITE 1
-#define BLACK 2
-
-// 원래는
-// const int NUM_ROWS = 8;
-// const int NUM_COLS = 8;
-
-
-//Funtions
-int determine_game_type_new_or_existing();
-void initialize_game_board(int gb [][NUM_COLS]);
-//void put_piece(int gb [][NUM_COLS], int player);
-bool check_pull_board(int gb [][NUM_COLS]);
-int switchplayer (int player);
-int checkAdjacency(int gb [][NUM_COLS], int r, int c, int dir, int player);
-void set(int gb [][NUM_COLS], int r, int c, int ply, int dir, int res);
-
-int main()
+void MainWindow::on_ending_button_clicked()
 {
-    int gameboard [NUM_ROWS][NUM_COLS];
-    //잘 모르겠음
-    int player;
-
-    initialize_game_board(gameboard);
-    player = 1; //첫번째 플레이어
-    while(check_pull_board(gameboard)){
-        //
-        int row, col;
-        cout << "player, Where should I put the piece?";
-        cout << "row : " << endl;
-        cin >> row;
-        cout << "col : " << endl;
-        cin >> col;
-        //row, col = put_piece(gameboard, player);
-
-        int adjacency=0;
-
-        for(int dir=0; dir<8; dir++){
-         int res=checkAdjacency(gameboard, row, col, dir, player);
-         if(res>0){
-          adjacency+=res;
-
-          //둘러싸고 있는 돌들은 우리 돌로 바꾸기.
-          set(gameboard, row, col, player, dir, res);
-         }
-        }
-        player = switchplayer(player);
-
-    }
-    for(int i = 0; i < NUM_ROWS; i++){
-        for (int j = 0; j < NUM_COLS; j++){
-            cout << gameboard[i][j] << " ";
-        }
-        cout << endl;
-    }
-
+    end_the_game();
 }
 
 
-void initialize_game_board(int gb [][NUM_COLS]) //done using ui
+void MainWindow::on_normal_mode_clicked()
 {
-    for (int i = 0; i < NUM_ROWS; i++)
-    {
-        for(int j = 0; j < NUM_COLS; j++)
-            gb[i][j] = EMPTY;
-    }
-    gb[3][3] = WHITE; //1:white
-    gb[4][4] = WHITE;
-    gb[3][4] = BLACK; //2:black
-    gb[4][3] = BLACK;
+    ui->stackedWidget->setCurrentIndex(2);
 }
 
-bool check_pull_board(int gb [][NUM_COLS])
+
+void MainWindow::on_limited_mode_clicked()
 {
-    int n_empty = 0;
-    for (int i = 0; i < NUM_ROWS; i++){
-        for(int j = 0; j < NUM_COLS; j++){
-            if(gb[i][j]==EMPTY)
-                n_empty += 1;
-        }
-    }
-
-    if(n_empty == 0){
-        return false;
-    }else{
-        return true;
-    }
+    ui->stackedWidget->setCurrentIndex(2);
+    limit_mode=true;
 }
 
-int switchplayer (int player) { //switched using turn value
-    if (player == 1)
-    {
-        player =2;
-    }
-    else
-    {
-        player=1;
-    }
-    return(player);
-}
-
-int checkAdjacency(int gb [][NUM_COLS], int r, int c, int dir, int ply)
-{
- //dir은 8방향을 의미 : 0~7까지 유효하다.
-    int nOppCount=0;
-    int nMyCount=0;
-
-    while(1)
-   {
-    switch(dir){
-    case 0:
-        c--;
-        break;
-    case 1:
-        c++;
-        break;
-    case 2:
-        r--;
-        break;
-    case 3:
-        r++;
-        break;
-    case 4:
-        r--; c--;
-        break;
-    case 5:
-        r++; c--;
-        break;
-    case 6:
-        r--; c++;
-        break;
-    case 7:
-        r++; c++;
-        break;
-    default:
-        break;
-  }
-
-    if(r<0 || c<0 || r>7 || c>7) //더이상 진행이 되지 않는다.
-        break;
-
-    if (ply==1)
-    {
-        gb[r][c] = BLACK;
-        nMyCount++;
-        break;
-    }else if (ply==2){
-        gb[r][c] = WHITE;
-        nOppCount++;
-    }else {
-        break;
-    }
-  }
-    if(nOppCount>0 && nMyCount==1)
-        return nOppCount;
-    else
-        return 0;
-}
-
-
-void set(int gb [][NUM_COLS], int r, int c, int ply, int dir, int res)
-{
-    int nCount=0;
-
-    while(1)
-    {
-        switch(dir){
-            case 0:
-                c--;
-                break;
-            case 1:
-                c++;
-                break;
-            case 2:
-                r--;
-                break;
-            case 3:
-                r++;
-                break;
-            case 4:
-                r--; c--;
-                break;
-            case 5:
-                r++; c--;
-                break;
-            case 6:
-                r--; c++;
-                break;
-            case 7:
-                r++; c++;
-                break;
-            default:
-                break;
-    }
-    if (ply==1)
-     {
-         gb[r][c] = BLACK;
-     }else{
-         gb[r][c] = WHITE;
-     }
-  nCount++;
-  if(nCount==res) break;
- }
-}
-
-
-*/
--- mainwindow.h
+++ mainwindow.h
@@ -36,6 +36,12 @@ private slots:
 
 
 
+    void on_ending_button_clicked();
+
+    void on_normal_mode_clicked();
+
+    void on_limited_mode_clicked();
+
 private:
     Ui::MainWindow *ui;
     void initialize_board(); //starting board
@@ -45,8 +51,14 @@ private:
     void change_pieces(int start_idx, int end_idx, int current_player, int direction, int row_idx, int column_idx); //change value of pieces
     void update_board_icons(); //change icons of pieces
     void update_board_score(); //count score
-    void end_the_game(int player1_score, int player2_score); //ending the game when board is full
+    void end_the_game(); //ending the game when board is full
     int button_slots[8][8]={0}; //initialize to zero
+
+    bool limit_mode=false; //when limit mode is on
+    int turn=0; //creating turns
+    int starting_player=0;
+    int player1_score=0; //scores declared as global so that game can end anytime and scores can be remembered
+    int player2_score=0;
 
 
 };
